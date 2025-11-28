3282f7581528be6d1455fe090ed7a69f9950cc89
hirock0 <hirockdutta0@gmail.com>
2025-11-28 11:38:48 +0600

    banner added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
-import Image from "next/image";
-
+export const dynamic = "force-dynamic";
+import Banner from "@/components/Home/Banner/Banner";
+import Products from "@/components/Home/Products/Products";
 export default function Home() {
-  return <div>Al-Shafi I have done</div>;
+  return (
+    <div>
+      <Banner/>
+      <div className=" max-w-[1440px] mx-auto px-5">
+        <div>
+          
+          <Products />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="w-full relative overflow-hidden ">
+      <img
+        src="https://res.cloudinary.com/dusp1j4e0/image/upload/v1764307818/Al-Safi/banners/web-Banner_utejvo.webp"
+        alt="banner"
+        className="w-full object-cover "
+      />
+    </div>
+  );
+};
+
+export default Banner;
