228ff34d7cafead0b36a9c223b31c0be8ae9cd7e
rubayat-rafi <rubayatrafi522@gmail.com>
2025-12-15 21:18:54 +0600

    chnage design

--- src/app/(adminRoutes)/admin-dashboard/layout.js
+++ src/app/(adminRoutes)/admin-dashboard/layout.js
@@ -1,22 +1,20 @@
-// export const dynamic = "force-dynamic";
-'use client'
+export const dynamic = "force-dynamic";
 import SideBar from "@/components/AdminDashboard/SideBar/SideBar";
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "@/components/AdminDashboard/Navbar/Navbar";
+import SidebarOverlay from "@/components/SidebarOverlay/SidebarOverlay";
 
 const Layout = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="flex">
         {/* Sidebar */}
-        <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <SideBar />
 
         {/* Main Content */}
         <main className="flex-1 lg:ml-64 w-full">
           {/* Navbar */}
-          <Navbar setSidebarOpen={setSidebarOpen} />
+          <Navbar />
 
           {/* Page Content */}
           <div className="p-4 md:p-6">{children}</div>
@@ -24,12 +22,13 @@ const Layout = ({ children }) => {
       </div>
 
       {/* Overlay for mobile */}
-      {sidebarOpen && (
+      <SidebarOverlay />
+      {/* {sidebarOpen && (
         <div
           className="fixed inset-0 bg-black/50 z-30 lg:hidden"
           onClick={() => setSidebarOpen(false)}
         />
-      )}
+      )} */}
     </div>
   );
 };
