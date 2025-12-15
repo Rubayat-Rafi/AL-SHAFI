7f0fd3165c07983e27d1ec0f475dfb0a97c4038f
hirock0 <hirockdutta0@gmail.com>
2025-12-15 23:03:33 +0600

    filter section

--- src/app/(publicRoutes)/product/collections/[slug]/page.js
+++ src/app/(publicRoutes)/product/collections/[slug]/page.js
@@ -1,8 +1,10 @@
 export const dynamic = "force-dynamic";
 import { CategoryWiseProducts } from "@/actions/actions";
+import FilterProducts from "@/components/AdminDashboard/Products/FilterProducts/FilterProducts";
 import ProductCard from "@/components/Products/ProductCard/ProductCard";
-const Collections = async ({ params }) => {
-  const { slug } = await params;
+const Collections = async (params) => {
+  const { grids } = await params?.searchParams;
+  const { slug } = await params?.params;
   const products = await CategoryWiseProducts(slug);
   if (!products || products.length === 0) {
     return (
@@ -13,8 +15,21 @@ const Collections = async ({ params }) => {
   }
   return (
     <div className="">
-      <h1 className="text-2xl font-bold mb-6 uppercase font-montserrat">{slug} Products</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <h1 className="text-2xl max-lg:hidden font-bold mb-6 uppercase font-montserrat">
+        {slug} Products
+      </h1>
+      <div className=" ">
+        <FilterProducts slug={slug} />
+      </div>
+      <div
+        className={`
+          ${
+            grids
+              ? `grid grid-cols-${grids}`
+              : "grid grid-cols-3 max-md:grid-cols-2"
+          } gap-6
+        `}
+      >
         {products.map((prod) => (
           <ProductCard key={prod._id} product={JSON.stringify(prod)} />
         ))}
